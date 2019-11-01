03a336126b2c815842a3233c9cf7928b0c56d2e0
Reza Karimi <rezakrimi@gmail.com>
2019-11-01 02:19:28 -0400

    init

---
+++ precache-manifest.f3257327b16f00f3f1c956339edd704f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138f24e7751887549687e928e07a7b61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "daf6cba1ec3b8936fd9a",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6674eb482872c3fbcdba",
+    "url": "./static/css/main.98c768a9.chunk.css"
+  },
+  {
+    "revision": "daf6cba1ec3b8936fd9a",
+    "url": "./static/js/2.c232373e.chunk.js"
+  },
+  {
+    "revision": "6674eb482872c3fbcdba",
+    "url": "./static/js/main.6c18ed48.chunk.js"
+  },
+  {
+    "revision": "b8c6ed0ef8ea26d55ecb",
+    "url": "./static/js/runtime-main.c5f9e74f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
